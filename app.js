9ceb6e3d9df229978b6e7d30b48290901cd74a08
Komathissivakumar <76860922+Komathissivakumar@users.noreply.github.com>
2021-02-01 18:20:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+ const hamburger = document.querySelector('header .nav-bar .nav-list .hamburger');
+  const mobile_menu = document.querySelector('header .nav-bar .nav-list .ul');
+  const mobile_item = document.querySelectorAll('header .nav-bar .nav-list .ul li a');
+const header=document.querySelector('.header.container');
+    
+	hamburger.addEventListener('click',()=>{
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+document.addEventListener('scroll',()=>{
+	var scroll_postion=window.scrollY;
+	if(scroll_postion>250)
+	{
+		header.style.backgroundColor='#29323c';
+	}
+	else{
+		header.style.backgroundColor='transparent';
+}});
+menu-list.forEach((item)=>{
+	item.addEventListener('click',()=>{
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
